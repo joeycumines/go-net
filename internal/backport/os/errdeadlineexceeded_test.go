golang/net

commit 540bb53d3b2ef1be1c9bd6bffb0281fc6c7038ca

http2: add Transport.WriteByteTimeout
Add a Transport-level knob to set a timeout for writes to net.Conns.
If a write exceeds the timeout without making any progress (at least
one byte written), the connection is closed.

Fixes golang/go#48830.

Change-Id: If0f57996d11c92bced30e07d1e238cbf8994acb4
Reviewed-on: https://go-review.googlesource.com/c/net/+/354431
Trust: Damien Neil <dneil@google.com>
Run-TryBot: Damien Neil <dneil@google.com>
TryBot-Result: Go Bot <gobot@golang.org>
Reviewed-by: Brad Fitzpatrick <bradfitz@golang.org>

http2/transport.go
@@ -24,6 +24,7 @@ import (
 	"net/http"
 	"net/http/httptrace"
 	"net/textproto"
+	"os"
 	"sort"
 	"strconv"
 	"strings"
@@ -129,6 +130,11 @@ type Transport struct {
 	// if a response to Ping is not received.
 	// Defaults to 15s.
 	PingTimeout time.Duration
+
+	// WriteByteTimeout is the timeout after which the connection will be
+	// closed no data can be written to it. The timeout begins when data is
+	// available to write, and is extended whenever any bytes are written.
+	WriteByteTimeout time.Duration
 
 	// CountError, if non-nil, is called on HTTP/2 transport errors.
 	// It's intended to increment a metric for monitoring, such
@@ -393,17 +399,31 @@ func (cs *clientStream) abortRequestBodyWrite() {
 }
 
 type stickyErrWriter struct {
-	w   io.Writer
-	err *error
+	conn    net.Conn
+	timeout time.Duration
+	err     *error
 }
 
 func (sew stickyErrWriter) Write(p []byte) (n int, err error) {
 	if *sew.err != nil {
 		return 0, *sew.err
 	}
-	n, err = sew.w.Write(p)
-	*sew.err = err
-	return
+	for {
+		if sew.timeout != 0 {
+			sew.conn.SetWriteDeadline(time.Now().Add(sew.timeout))
+		}
+		nn, err := sew.conn.Write(p[n:])
+		n += nn
+		if n < len(p) && nn > 0 && errors.Is(err, os.ErrDeadlineExceeded) {
+			// Keep extending the deadline so long as we're making progress.
+			continue
+		}
+		if sew.timeout != 0 {
+			sew.conn.SetWriteDeadline(time.Time{})
+		}
+		*sew.err = err
+		return n, err
+	}
 }
 
 // noCachedConnError is the concrete type of ErrNoCachedConn, which
@@ -658,7 +678,11 @@ func (t *Transport) newClientConn(c net.Conn, singleUse bool) (*ClientConn, erro
 
 	// TODO: adjust this writer size to account for frame size +
 	// MTU + crypto/tls record padding.
-	cc.bw = bufio.NewWriter(stickyErrWriter{c, &cc.werr})
+	cc.bw = bufio.NewWriter(stickyErrWriter{
+		conn:    c,
+		timeout: t.WriteByteTimeout,
+		err:     &cc.werr,
+	})
 	cc.br = bufio.NewReader(c)
 	cc.fr = NewFramer(cc.bw, cc.br)
 	if t.CountError != nil {

http2/transport_test.go
@@ -5736,3 +5736,73 @@ func TestTransport300ResponseBody(t *testing.T) {
 	res.Body.Close()
 	pw.Close()
 }
+
+func TestTransportWriteByteTimeout(t *testing.T) {
+	st := newServerTester(t,
+		func(w http.ResponseWriter, r *http.Request) {},
+		optOnlyServer,
+	)
+	defer st.Close()
+	tr := &Transport{
+		TLSClientConfig: tlsConfigInsecure,
+		DialTLS: func(network, addr string, cfg *tls.Config) (net.Conn, error) {
+			_, c := net.Pipe()
+			return c, nil
+		},
+		WriteByteTimeout: 1 * time.Millisecond,
+	}
+	defer tr.CloseIdleConnections()
+	c := &http.Client{Transport: tr}
+
+	_, err := c.Get(st.ts.URL)
+	if !errors.Is(err, os.ErrDeadlineExceeded) {
+		t.Fatalf("Get on unresponsive connection: got %q; want ErrDeadlineExceeded", err)
+	}
+}
+
+type slowWriteConn struct {
+	net.Conn
+	hasWriteDeadline bool
+}
+
+func (c *slowWriteConn) SetWriteDeadline(t time.Time) error {
+	c.hasWriteDeadline = !t.IsZero()
+	return nil
+}
+
+func (c *slowWriteConn) Write(b []byte) (n int, err error) {
+	if c.hasWriteDeadline && len(b) > 1 {
+		n, err = c.Conn.Write(b[:1])
+		if err != nil {
+			return n, err
+		}
+		return n, fmt.Errorf("slow write: %w", os.ErrDeadlineExceeded)
+	}
+	return c.Conn.Write(b)
+}
+
+func TestTransportSlowWrites(t *testing.T) {
+	st := newServerTester(t,
+		func(w http.ResponseWriter, r *http.Request) {},
+		optOnlyServer,
+	)
+	defer st.Close()
+	tr := &Transport{
+		TLSClientConfig: tlsConfigInsecure,
+		DialTLS: func(network, addr string, cfg *tls.Config) (net.Conn, error) {
+			cfg.InsecureSkipVerify = true
+			c, err := tls.Dial(network, addr, cfg)
+			return &slowWriteConn{Conn: c}, err
+		},
+		WriteByteTimeout: 1 * time.Millisecond,
+	}
+	defer tr.CloseIdleConnections()
+	c := &http.Client{Transport: tr}
+
+	const bodySize = 1 << 20
+	resp, err := c.Post(st.ts.URL, "text/foo", io.LimitReader(neverEnding('A'), bodySize))
+	if err != nil {
+		t.Fatal(err)
+	}
+	resp.Body.Close()
+}

joeycumines/go-net

commit 51aa45c9f3b3a0a3fe4b80125630c2e2ed8acc6b

backport(os.ErrDeadlineExceeded): for go 1.14 support
The breaking change was introduced in
540bb53d3b2ef1be1c9bd6bffb0281fc6c7038ca

http2/transport.go
@@ -24,7 +24,6 @@ import (
 	"net/http"
 	"net/http/httptrace"
 	"net/textproto"
-	"os"
 	"sort"
 	"strconv"
 	"strings"
@@ -35,6 +34,8 @@ import (
 	"golang.org/x/net/http/httpguts"
 	"golang.org/x/net/http2/hpack"
 	"golang.org/x/net/idna"
+	"golang.org/x/net/internal/backport"
+	osBackport "golang.org/x/net/internal/backport/os"
 )
 
 const (
@@ -414,7 +415,7 @@ func (sew stickyErrWriter) Write(p []byte) (n int, err error) {
 		}
 		nn, err := sew.conn.Write(p[n:])
 		n += nn
-		if n < len(p) && nn > 0 && errors.Is(err, os.ErrDeadlineExceeded) {
+		if n < len(p) && nn > 0 && backport.ErrorIs(err, osBackport.ErrDeadlineExceeded) {
 			// Keep extending the deadline so long as we're making progress.
 			continue
 		}

internal/backport/errors.go
@@ -1,0 +1,31 @@
+// Copyright 2022 The Go Authors.
+// Copyright 2022 Joseph Cumines.
+//
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package backport
+
+import (
+	"errors"
+)
+
+// ErrorIs performs a normal errors.Is then, if false, checks target.Backport_Is against every layer of err
+func ErrorIs(err error, target error) bool {
+	if errors.Is(err, target) {
+		return true
+	}
+	t, ok := target.(interface{ Backport_Is(err error) bool })
+	if !ok {
+		return false
+	}
+	for {
+		if t.Backport_Is(err) {
+			return true
+		}
+		if err = errors.Unwrap(err); err == nil {
+			return false
+		}
+	}
+}

internal/backport/os/errdeadlineexceeded_go114.go
@@ -1,0 +1,37 @@
+// Copyright 2022 The Go Authors.
+// Copyright 2022 Joseph Cumines.
+//
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build go1.14
+// +build go1.14
+
+package os
+
+import (
+	"net"
+)
+
+type (
+	// deadlineExceededError matches the behavior of internal/poll/fd.DeadlineExceededError
+	deadlineExceededError struct{}
+)
+
+var (
+	// ErrDeadlineExceeded was added in
+	// https://github.com/golang/go/commit/d422f54619b5b6e6301eaa3e9f22cfa7b65063c8
+	ErrDeadlineExceeded error = &deadlineExceededError{}
+)
+
+func (x *deadlineExceededError) Error() string   { return "i/o timeout" }
+func (x *deadlineExceededError) Timeout() bool   { return true }
+func (x *deadlineExceededError) Temporary() bool { return true }
+func (x *deadlineExceededError) Backport_Is(err error) bool {
+	if err, ok := err.(net.Error); ok && x.Timeout() == err.Timeout() && x.Temporary() == err.Temporary() {
+		// there's not much else we can do here (the reason for the change in the first place)
+		return true
+	}
+	return false
+}

internal/backport/os/errdeadlineexceeded_go115.go
@@ -1,0 +1,15 @@
+// Copyright 2020 The Go Authors.
+// Copyright 2022 Joseph Cumines.
+//
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:build !go1.14
+// +build !go1.14
+
+package os
+
+var (
+	ErrDeadlineExceeded = os.ErrDeadlineExceeded
+)

http2/transport_test.go
@@ -35,6 +35,8 @@ import (
 	"time"
 
 	"golang.org/x/net/http2/hpack"
+	"golang.org/x/net/internal/backport"
+	osBackport "golang.org/x/net/internal/backport/os"
 )
 
 var (
@@ -5867,7 +5869,7 @@ func TestTransportWriteByteTimeout(t *testing.T) {
 	c := &http.Client{Transport: tr}
 
 	_, err := c.Get(st.ts.URL)
-	if !errors.Is(err, os.ErrDeadlineExceeded) {
+	if !backport.ErrorIs(err, osBackport.ErrDeadlineExceeded) {
 		t.Fatalf("Get on unresponsive connection: got %q; want ErrDeadlineExceeded", err)
 	}
 }
@@ -5888,7 +5890,7 @@ func (c *slowWriteConn) Write(b []byte) (n int, err error) {
 		if err != nil {
 			return n, err
 		}
-		return n, fmt.Errorf("slow write: %w", os.ErrDeadlineExceeded)
+		return n, fmt.Errorf("slow write: %w", osBackport.ErrDeadlineExceeded)
 	}
 	return c.Conn.Write(b)
 }

internal/backport/os/errdeadlineexceeded_test.go
@@ -1,0 +1,79 @@
+// Copyright 2022 The Go Authors.
+// Copyright 2022 Joseph Cumines.
+//
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package os
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"golang.org/x/net/internal/backport"
+	"net"
+	"testing"
+	"time"
+)
+
+func TestErrDeadlineExceeded_is(t *testing.T) {
+	for _, tc := range [...]struct {
+		Name       string
+		Err        func(t *testing.T) error
+		IsErrors   bool
+		IsBackport bool
+	}{
+		{
+			Name:       `context deadline exceeded`,
+			Err:        func(t *testing.T) error { return context.DeadlineExceeded },
+			IsBackport: true,
+		},
+		{
+			Name: `pipe timeout error`,
+			Err: func(t *testing.T) error {
+				pipe, _ := net.Pipe()
+				defer pipe.Close()
+				if err := pipe.SetReadDeadline(time.Now()); err != nil {
+					t.Fatal(err)
+				}
+				_, err := pipe.Read(make([]byte, 1))
+				if err == nil {
+					panic(`expected error`)
+				}
+				return err
+			},
+			IsBackport: true,
+		},
+		{
+			Name: `poll errtimeout go114`,
+			Err: func(t *testing.T) error {
+				ctx, cancel := context.WithTimeout(context.Background(), 1)
+				defer cancel()
+				<-ctx.Done()
+				_, err := (&net.Dialer{}).DialContext(ctx, `tcp`, `localhost:0`)
+				if err == nil {
+					t.Fatal(`expected error`)
+				}
+				return err
+			},
+			IsBackport: true,
+		},
+	} {
+		t.Run(tc.Name, func(t *testing.T) {
+			err := tc.Err(t)
+			for e := err; e != nil; e = errors.Unwrap(e) {
+				t.Logf("%T %s\n", e, e)
+			}
+			if errors.Is(err, ErrDeadlineExceeded) != tc.IsErrors {
+				t.Error(err)
+			}
+			if backport.ErrorIs(err, ErrDeadlineExceeded) != tc.IsBackport {
+				t.Error(err)
+			}
+			if backport.ErrorIs(fmt.Errorf(`wrapped: %w`, err), ErrDeadlineExceeded) != tc.IsBackport {
+				t.Error(err)
+			}
+		})
+	}
+}